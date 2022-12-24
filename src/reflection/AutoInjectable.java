6a404ba6b3c19a07b6b52fe8133d0928c27c248a
maxim1770 <maxim1770@gmail.com>
2022-12-24 16:00:40 +0300

    Initial commit

---
+++ src/reflection/AutoInjectable.java
@@ -0,0 +1,12 @@
+package reflection;
+
+import java.lang.annotation.*;
+
+// Декларация аннотации "AutoInjectable"
+// Аннотация применима к полям классов
+@Target(ElementType.FIELD)
+// Аннотация будет доступна на стадии выполнения 
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AutoInjectable {
+
+}
